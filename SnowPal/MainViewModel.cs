5fd6c889355e326e6760ef99d9f7e8fbc35c7798
wakchu <stfnbrdn@gmail.com>
2025-10-04 00:20:37 +0200

    Initial commmit

---
+++ SnowPal/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SnowPal.Models;
+
+namespace SnowPal
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly Game _game;
+
+
+        // Properties bound to the UI
+        [ObservableProperty]
+        public partial string WordDisplay { get; set; }
+
+
+        // Constructor initializes the game and letters
+        public MainViewModel()
+        {
+            _game = new Game();
+            StartNewGame();
+        }
+
+        // Starts a new game and updates the properties
+        public void StartNewGame()
+        {
+
+            UpdateProperties();
+        }
+
+        // Command executed when a letter is guessed
+
+
+        // Ends the game, disables letters, and shows the end game message
+
+
+        // Command executed when the popup close button is clicked
+
+
+        // Shows the end game message in a popup
+
+
+        // Updates the properties bound to the UI
+        private void UpdateProperties()
+        {
+            WordDisplay = string.Join(" ", _game.GuessedWord);
+
+
+        }
+
+        // Enables or disables the letter buttons
+
+
+    }
+}
---
+++ SnowPal/Models/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnowPal.Models
+{
+    public class Game
+    {
+        // List of words for the game
+        private readonly List<string> _wordList = new List<string> { "WINDOWS", "VIEW", "MODEL", "TASKBAR", "XAML", "CSHARP", "DEBUGGER", "GRID", "STACKPANEL", "RANDOM" };
+
+
+        // Messages for winning and losing the game
+
+        // Properties for the current game state
+        public string CurrentWord { get; private set; }
+        public char[] GuessedWord { get; private set; } = [];
+
+        public Game()
+        {
+            StartNewGame();
+        }
+
+        // Starts a new game by selecting a random word and resetting the game state
+        public void StartNewGame()
+        {
+            var random = new Random();
+            CurrentWord = _wordList[random.Next(_wordList.Count)];
+            GuessedWord = new string('_', CurrentWord.Length).ToCharArray();
+
+        }
+
+        // Plays the game by guessing a letter and checking the game status
+
+
+        // Returns the current guessed word as a string with spaces between letters
+
+
+        // Guesses a letter and updates the guessed word and incorrect guesses count
+
+
+        // Checks the game status to determine if the game is won or lost
+
+
+        // Returns a winning message based on the number of guesses left
+
+
+        // Returns a random losing message
+    }
+}
