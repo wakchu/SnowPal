0aebfe6d4f85774aec96ae80d8ea74f70efa0ae3
wakchu <stfnbrdn@gmail.com>
2025-10-06 00:13:24 +0200

    Minor fixes

--- SnowPal/App.xaml.cs
+++ SnowPal/App.xaml.cs
@@ -16,6 +16,9 @@ using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+using Microsoft.UI.Xaml;
+
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -27,6 +30,16 @@ namespace SnowPal
     public partial class App : Application
     {
         private Window? _window;
+        public static ElementTheme CurrentTheme { get; private set; } = ElementTheme.Light;
+
+        public static void SetTheme(ElementTheme theme)
+        {
+            if ((Current as App)?._window?.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = theme;
+                CurrentTheme = theme;
+            }
+        }
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -45,6 +58,7 @@ namespace SnowPal
         {
             _window = new MainWindow();
             _window.Activate();
+            SetTheme(CurrentTheme);
         }
     }
 }
